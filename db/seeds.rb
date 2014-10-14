f7a588f4056123500afb14736649935619504ce3
Dary Merckens <dary@gunnertech.com>
2014-10-13 21:47:07 -0700

    seed it up

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 challenger=User.create!(mobile:"1231231234", username:"challenger")
 challengee=User.create!(mobile:"2342342345", username:"challengee")
 follower=User.create!(mobile:"3453453456", username:"follower")
