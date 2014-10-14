742e173e7619718c2064d55f96ce359380c2c4fd
Dary Merckens <dary@gunnertech.com>
2014-10-13 21:46:31 -0700

    seed it up

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+challenger=User.create!(mobile:"1231231234", username:"challenger")
+challengee=User.create!(mobile:"2342342345", username:"challengee")
+follower=User.create!(mobile:"3453453456", username:"follower")
+watcher=User.create!(mobile:"4564564567", username:"watcher")
+
+tw1=Thumbwar.create!(challenger: challenger, challengee: challengee, body: "1")
+tw2=Thumbwar.create!(challenger: challenger, challengee: challengee, body: "2")
+tw3=Thumbwar.create!(challenger: challenger, challengee: challengee, body: "3")
+
+challenger.followers << follower
+tw1.watchers << watcher
+
+tw1.update_attribute(:winner_id, challenger.id)
