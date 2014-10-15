26663a2689c2f46b69390a02a6921aa14c64b261
Dary Merckens <dary@gunnertech.com>
2014-10-14 19:32:35 -0700

    Update alerts_controller.rb

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -5,7 +5,7 @@ class AlertsController < InheritedResources::Base
   def collection
   	return @alerts if @alerts
 
-  	@alerts = end_of_association_chain.where{ user_id == my{@current_user.id}}
+  	@alerts = end_of_association_chain.where{ user_id == my{@current_user.id} }
 
   	@alerts = @alerts.where{ read == my{params[:read]} } if params[:read].present?
 
