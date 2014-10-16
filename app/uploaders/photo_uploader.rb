9cbbf178a3e843729e38591f75e030b91a4f617a
Cody Swann <cody@gunnertech.com>
2014-10-16 13:31:28 -0400

    bet creator can add a thumbwar photo via clicking existing image or clicking a comment image

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -4,7 +4,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
@@ -17,12 +17,17 @@ class PhotoUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  def default_url
+    "#{ENV['HOST']}/images/fallback/photos/" + [version_name, "default.png"].compact.join('_')
+  end
+
+  version :large do
+    process :resize_to_fill => [1280,860]
+  end
+
+  version :small do
+    process :resize_to_fill => [320,215]
+  end
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
