db909cf96825a13c17339063bd7ffbcf75a532e4
Cody Swann <cody@gunnertech.com>
2014-10-24 22:17:45 -0400

    minor

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -3,7 +3,7 @@ class AlertsController < InheritedResources::Base
   
 
   def collection
-  	return @alerts if @alerts
+    # return @alerts if @alerts
 
   	@alerts = end_of_association_chain.where{ user_id == my{@current_user.id} }
 
