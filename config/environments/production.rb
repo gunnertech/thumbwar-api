b96cfce9bc17797f869b414475d8e2365a35b590
Cody Swann <cody@gunnertech.com>
2014-10-28 19:39:16 -0400

    Rails 4 tweaks - add logging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,6 +12,12 @@ Rails.application.configure do
   config.log_level = :info
   config.log_formatter = ::Logger::Formatter.new
   
+  logger = Logger.new(STDOUT)
+  logger = ActiveSupport::TaggedLogging.new(logger) if defined?(ActiveSupport::TaggedLogging)
+  config.logger = logger
+  log_level_env_override = Logger.const_get(ENV['LOG_LEVEL'].try(:upcase)) rescue nil
+  config.logger.level = log_level_env_override || Logger.const_get(Rails.configuration.log_level.to_s.upcase)
+  
   config.i18n.fallbacks = true
   
   config.active_support.deprecation = :notify
