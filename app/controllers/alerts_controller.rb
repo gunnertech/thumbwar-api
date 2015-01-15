553a6f0cc53b4d1ed2a79884268b0cea12020b08
Cody Swann <cody@gunnertech.com>
2015-01-14 19:48:44 -0500

    temp

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -8,6 +8,8 @@ class AlertsController < InheritedResources::Base
   	@alerts = end_of_association_chain.where{ user_id == my{@current_user.id} }
 
   	@alerts = @alerts.where{ read == my{params[:read]} } if params[:read].present?
+    
+    @result_count = @alerts.count
 
   	@alerts.reorder{ id.desc }
   end
