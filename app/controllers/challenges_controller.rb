9351a5505e5127666f944d46c2eb7215cd0d3e31
Cody Swann <cody@gunnertech.com>
2015-01-18 11:08:12 -0500

    adds comment api

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -1,9 +1,9 @@
 class ChallengesController < InheritedResources::Base
-  prepend_before_filter :set_user_id
+  before_filter :set_challenger_id, only: :create
 
   protected
   
   def set_user_id
-    params[:challenge][:user_id] = current_user.id
+    params[:challenge][:challenger_id] = current_user.id
   end
 end
