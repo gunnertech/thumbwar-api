a0a75c8602e221c05e85d29a10ad6b5f125da68a
Cody Swann <cody@gunnertech.com>
2015-01-21 17:56:32 -0500

    adds comment api

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,2 +1,7 @@
 class DevicesController < InheritedResources::Base
+  before_filter :set_challenger_id, only: :create
+  
+  def set_user_id
+    params[:device][:user_id] = current_user.id
+  end
 end
