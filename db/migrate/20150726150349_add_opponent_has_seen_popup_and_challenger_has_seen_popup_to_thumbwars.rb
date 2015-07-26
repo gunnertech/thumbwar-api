57b6842433b175eeb5583b45fd821d1cf7014a97
Cody Swann <cody@gunnertech.com>
2015-07-26 11:06:43 -0400

    adds popup flag

---
+++ db/migrate/20150726150349_add_opponent_has_seen_popup_and_challenger_has_seen_popup_to_thumbwars.rb
@@ -0,0 +1,6 @@
+class AddOpponentHasSeenPopupAndChallengerHasSeenPopupToThumbwars < ActiveRecord::Migration
+  def change
+    add_column :thumbwars, :opponent_has_seen_popup, :boolean, default: false
+    add_column :thumbwars, :challenger_has_seen_popup, :boolean, default: false
+  end
+end
