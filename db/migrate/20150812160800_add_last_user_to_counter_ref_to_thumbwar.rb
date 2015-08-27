688f7ac4bca48a376edfc2148689b91748f35f5f
Mahdi Hussein <m@hdihusse.in>
2015-08-26 21:19:26 -0400

    Fix Migration

--- db/migrate/20150812160800_add_last_user_to_counter_ref_to_thumbwar.rb
+++ db/migrate/20150812160800_add_last_user_to_counter_ref_to_thumbwar.rb
@@ -1,6 +1,6 @@
 class AddLastUserToCounterRefToThumbwar < ActiveRecord::Migration
   def change
-    add_reference :thumbwars, :challenger_has_seen_popup, index: true, foreign_key: true
+    add_reference :thumbwars, :last_user_to_counter, index: true, foreign_key: true
   end
 end
 
